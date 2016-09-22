6885f68d1e7f9d80594c26150b1ee001f8ec13e0
Anthony Stathers <astathers@gmail.com>
2016-09-22 22:48:32 +0100

    Create basic implementation and cmake build

---
+++ src/maybe.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <random>
+
+int main() {
+  std::random_device random_device;
+  std::mt19937_64 generator(random_device());
+  std::uniform_int_distribution<unsigned int> distribution;
+
+  char outputs[2] = { 'y', 'n' };
+
+  if (random_device.entropy() == 0) {
+    std::cerr << "Maybe: warning - non-deterministic random"
+      " source not available" << std::endl;
+  }
+  
+  while(std::cout.good()) {
+    unsigned int random_uint = distribution(generator);
+    for (unsigned int bit_idx = 0; bit_idx < sizeof(unsigned int); ++bit_idx) {
+      std::cout << outputs[!!(random_uint & (1 << bit_idx))] << '\n';
+    }
+    std::cout.flush();
+  }
+
+  if (std::cout.rdstate() & std::ostream::eofbit) {
+    return 0;
+  }
+
+  std::cerr << "Maybe: error writing to output; aborting." << std::endl;
+  return 1;
+}
